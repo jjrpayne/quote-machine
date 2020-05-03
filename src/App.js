2171ec0b260df395772f629193324e8eb97c70aa
John Payne <jjrpayne@uvic.ca>
2020-05-02 18:00:55 -0700

    added random quote functionality

--- src/App.js
+++ src/App.js
@@ -9,39 +9,41 @@ class App extends Component {
 			quotes: [],
 			selectedQuoteIndex: null
 		}
+		this.assignQuoteIndex = this.assignQuoteIndex.bind(this);
 		this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
 	}
 
 	componentDidMount() {
+		// fetch quotes
 		fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
 			.then(data => data.json())
-			.then(mQuotes => this.setState({quotes: mQuotes}, () => {
-				this.setState({selectedQuoteIndex: this.selectQuoteIndex()})
-			}));
-	}
-
-	nextQuoteClickHandler()	{
-		console.log('hello there');
+			.then(mQuotes => this.setState({quotes: mQuotes}, this.assignQuoteIndex));
 	}
 
 	selectedQuote() {
+		// return the selected quote
 		if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex))
-			return;
+			return undefined;
 		else return this.state.quotes[this.state.selectedQuoteIndex];
 	}
 
 	selectQuoteIndex(){
+		// generate and return new quote index
 		if(!this.state.quotes.length)
-			return;
+			return undefined;
 		else
 			return Math.floor(Math.random()*(this.state.quotes.length));
+	}
+
+	assignQuoteIndex(){
+		this.setState({selectedQuoteIndex: this.selectQuoteIndex()});
 	}
 
 	render() { 
 		return (
     		<div className="App" id="quote-box">
 				{this.selectedQuote() ? `"${this.selectedQuote().quote}" - ${this.selectedQuote().author}` : ''}
-				<Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler}/>
+				<Button buttonDisplayName="Next Quote" clickHandler={this.assignQuoteIndex}/>
     		</div>
 		);
 	}
