f33fdda7a043aec25489e0d395c9ef6973fc0800
John Payne <jjrpayne@uvic.ca>
2020-05-04 20:56:53 -0700

    added twitter functionality

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Button from './Button';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 const QuoteMachine = (props) => (
-	<React.Fragment>
-		{props.selectedQuote ? `"${props.selectedQuote.quote}" - ${props.selectedQuote.author}` : ''}
-		<Button buttonDisplayName="Next Quote" clickHandler={props.assignQuoteIndex}/>
-	</React.Fragment>
+	<Card>
+		<CardContent>
+			<Typography>
+				{props.selectedQuote.quote} - {props.selectedQuote.author}
+			</Typography>
+		</CardContent>
+		<CardActions>
+			<Button onClick={props.assignQuoteIndex}>Next Quote</Button>
+			<IconButton
+				target='_blank'
+				href={`https://twitter.com/intent/tweet?text=${props.selectedQuote.quote}`}
+			>
+				<FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+			</IconButton>
+		</CardActions>
+	</Card>
 );
 
 export default QuoteMachine
